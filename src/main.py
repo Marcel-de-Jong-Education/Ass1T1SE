16f6599097976ed4375c065a000738e9fb67b0cd
Marcel-de-Jong-Education <marcel.dejong546@education.nsw.gov.au>
2025-03-10 11:09:31 +1100

    Its spaghetti!! <3

--- src/main.py
+++ src/main.py
@@ -1,6 +1,45 @@
 # https://dictionaryapi.dev/ #
+import os
+os.system('cls')
 import requests as r
-contents = r.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello").json()[0]
 
-for x in contents:
-  print(contents[x]) 
+while True:
+  wordChoice = input("Enter word:\n")
+  try: contents = r.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + wordChoice.lower()).json()[0]
+  except: print("Error 404 Word not found!"); continue
+
+  options = ""
+  for a in contents: options += a + ", "
+
+  group = input("Options:\n" + options[6:len(options)-2] + '\n')
+
+  things = []
+  appTo = False
+  tol = 0
+  rawG = str(contents[group])
+  for i in range(len(rawG)):
+      rawGinx = rawG[-(i+1)] # todo: make better
+
+      if appTo:
+          things[-1] += rawGinx
+
+      if rawGinx == ':':
+          appTo = True
+          things.append("")
+      elif rawGinx == "'":
+          tol += 1
+          if tol == 2:
+              appTo = False
+              tol = 0
+          
+  for thing in things:
+      print(thing[::-1], end=", ")
+
+  wantToFind = input("\nFind..?\n")
+  result = rawG[rawG.find(wantToFind) + len(wantToFind) + 4:]
+  for a in result:
+      #if a == "'": break
+      print(a, end='')
+
+  
+  if input("\nSatisfied? (Y/n)\n") == 'Y': break
