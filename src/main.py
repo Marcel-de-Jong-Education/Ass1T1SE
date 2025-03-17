c0f78bf4131e857e198c2bd3d264b8aa1cd20410
Marcel-de-Jong-Education <marcel.dejong546@education.nsw.gov.au>
2025-03-17 15:10:44 +1100

    restart for the better

--- src/main.py
+++ src/main.py
@@ -1,45 +1,22 @@
 # https://dictionaryapi.dev/ # API Utilised
 import os # To run terminal commands
-os.system('cls') # Clear the terminal before the programme runs
+#os.system('cls') # Clear the terminal before the programme runs
 import requests as r # To retrieve API data
 
 while True: # User can query as many words as they like without needing to re-start the programme each time
-  wordChoice = input("Enter word:\n") #
-  try: contents = r.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + wordChoice.lower()).json()[0] # Retrieve the raw API data
-  except: print("Error 404 Word not found!"); continue # Try again if malformed input is received.
+    wordChoice = input("Enter word:\n") #
+    try: contents = r.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + wordChoice.lower()).json()[0] # Retrieve the raw API data
+    except: print("Error 404 Word not found!"); continue # Try again if malformed input is received.
 
-  options = "" # 
-  for a in contents: options += a + ", " # Aquire all dictionary keys
+    options = "" # 
+    for a in contents: 
+        if (a != "sourceUrls" and a != "license"): options += a + ", " # Aquire all dictionary keys
 
-  group = input("Options:\n" + options[6:len(options)-2] + '\n') # The dictionary key the user wants
+    group = input("Options:\n" + options[6:len(options)-2] + '\n') # The dictionary key the user wants
 
-  things = []
-  appTo = False
-  tol = 0
-  rawG = str(contents[group])
-  for i in range(len(rawG)):
-      rawGinx = rawG[-(i+1)] # todo: make better
+    rawText = contents[group][:-2] # I have no idea whats going on but this somehow deletes like 100 useless characters off the end of the information so DONT TOUCH IT; ITS DEEP MAGIC
+    print(rawText)
 
-      if appTo:
-          things[-1] += rawGinx
 
-      if rawGinx == ':':
-          appTo = True
-          things.append("")
-      elif rawGinx == "'":
-          tol += 1
-          if tol == 2:
-              appTo = False
-              tol = 0
-          
-  for thing in things:
-      print(thing[::-1], end=", ")
 
-  wantToFind = input("\nFind..?\n")
-  result = rawG[rawG.find(wantToFind) + len(wantToFind) + 4:]
-  for a in result:
-      #if a == "'": break
-      print(a, end='')
-
-  
-  if input("\nSatisfied? (Y/n)\n") == 'Y': break
+    if input("\nSatisfied? (Y/n)\n") == 'Y': break
