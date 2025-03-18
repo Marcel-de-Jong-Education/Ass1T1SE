bc72ec71d43d70d393ecfd89b9b4f05ce10585d9
Marcel-de-Jong-Education <marcel.dejong546@education.nsw.gov.au>
2025-03-19 09:38:40 +1100

    Im done.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # https://dictionaryapi.dev/ # API Utilised
 import os # To run terminal commands
-#os.system('cls') # Clear the terminal before the programme runs
+os.system('cls') # Clear the terminal before the programme runs
 import requests as r # To retrieve API data
 
 while True: # User can query as many words as they like without needing to re-start the programme each time
@@ -12,10 +12,20 @@ while True: # User can query as many words as they like without needing to re-st
     for a in contents: 
         if (a != "sourceUrls" and a != "license"): options += a + ", " # Aquire all dictionary keys
 
-    group = input("Options:\n" + options[6:len(options)-2] + '\n') # The dictionary key the user wants
+    group = input("\nOptions:\n" + options[6:len(options)-2] + '\n') # The dictionary key the user wants
 
-    rawText = contents[group][:-2] # I have no idea whats going on but this somehow deletes like 100 useless characters off the end of the information so DONT TOUCH IT; ITS DEEP MAGIC
-    print(rawText)
+    try: rawText = contents[group][:-(len(contents[group])-1)][0] # I have no idea whats going on but this somehow deletes like 100 useless characters off the end of the information so DONT TOUCH IT; ITS DEEP MAGIC
+    except: print("Contents inextractable!"); continue
+
+    print(); 
+
+    for a in rawText:
+        if (a != "license"):
+            if (type(a) is str):
+                print(a + ':', rawText[a])
+            else:
+                print(str(a) + ':', rawText)
+
 
 
 
