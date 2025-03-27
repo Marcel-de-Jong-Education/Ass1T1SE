cab58f910653d73389ffd3d6a6283d9917a87222
Marcel-de-Jong-Education <marcel.dejong546@education.nsw.gov.au>
2025-03-27 15:10:10 +1100

    text colours!

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 import os; # To run terminal commands
 os.system('cls'); # Clear the terminal before the programme runs
 import requests as r; # To retrieve API data
-import color as colour
+from colorama import Fore
 
 print( # Idk why but the code breaks without this
     r"""   ,--"""",--.__,---[],-------._         """ + '\n' +
@@ -45,35 +45,37 @@ print( # Idk why but the code breaks without this
     r"""              `._::,.:,.:,:_ctr_:,:,.::,.:_;'` """+ '\n' +
     r"  ________________`" + '"' + str("\\") + "/" + '"' + str("\\") + '/' + str("\\") + '/' + """ '"""" """ + '`' + str("\\") + "/" + '"' + str("\\") + "/" + '"' + '"' + str("\\") + "/" + '"' + "____________________________ "
     ,end = "\n\n\n\n"
-)
+    )
 
 while True: # User can query as many words as they like without needing to re-start the programme each time
     print(
+        Fore.LIGHTMAGENTA_EX +
         "Query History:\n✨————————————————✧—♥—✧————————————————✨" # Powershell is utf-8 encoded, this is totally fine!
         + open(os.getcwd()[:-3] + "\\user_history\\guest_history.txt",'r').read(),
         end="\n✨————————————————✧♥✧————————————————✨\n"
         )
 
-    wordChoice = input("Enter word:\n"); #
+    wordChoice = input("Enter word:\n" + Fore.WHITE); #
     open(os.getcwd()[:-3] + "\\user_history\\guest_history.txt",'a').write('\n'+wordChoice) # Will save requests even if they're invalid. and thats okay!! <3
          
     try: contents = r.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + wordChoice.lower()).json()[0]; # Retrieve the raw API data
-    except: print("Error 404 Word not found!"); continue # Try again if malformed input is received.
+    except: print(Fore.RED + "Error 404 Word not found!" + Fore.WHITE); continue # Try again if malformed input is received.
 
     options = ""; # 
     for a in contents: 
         if (a != "sourceUrls" and a != "license"): options += a + ", "; # Aquire all dictionary keys
 
-    group = input("\nOptions:\n" + options[6:len(options)-2] + '\n'); # The dictionary key the user wants
+    group = input(Fore.LIGHTMAGENTA_EX + "\nOptions:\n" + options[6:len(options)-2] + '\n' + Fore.WHITE); # The dictionary key the user wants
 
     if (group == "phonetic"):
-        print(contents[group])
+        print(Fore.LIGHTMAGENTA_EX + contents[group])
+        if input("\nSatisfied? (Y/n)\n" + Fore.WHITE) == 'Y':break; # Escape!!!!!!
         continue; 
 
     try: rawText = contents[group][:-(len(contents[group])-1)][0] # I have no idea whats going on but this somehow deletes like 100 useless characters off the end of the information so DONT TOUCH IT; ITS DEEP MAGIC
-    except: print("Contents inextractable!"); continue # Not going to deal with it if its not the most common case
+    except: print(Fore.RED + "Contents inextractable!" + Fore.WHITE); continue # Not going to deal with it if its not the most common case
 
-    print();  # For aesthetics, obviously, we need to be 可愛い～！
+    print(Fore.LIGHTMAGENTA_EX);  # For aesthetics, obviously, we need to be 可愛い～！
     if (type(rawText) == dict):
         for a in rawText: # Iterate through all the dictionary keys~
             if (a != "license"): # No-one cares about the license!
@@ -85,4 +87,4 @@ while True: # User can query as many words as they like without needing to re-st
                 
 
 
-    if input("\nSatisfied? (Y/n)\n") == 'Y': break; # Escape!!!!!!
+    if input("\nSatisfied? (Y/n)\n" + Fore.WHITE) == 'Y':break; # Escape!!!!!!
